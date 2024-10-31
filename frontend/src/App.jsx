01cbfa2d74531f6197736219a60a6a4734908036
baki duman <g.s_baki_70@hotmail.com>
2024-10-31 11:45:08 +0300

    Profile page added.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <>Home </>;
+  return (
+    <>
+      <div className="bg-black text-white min-h-screen flex justify-center">
+        <div className="w-[620px]">
+          <header className="flex justify-center pt-6 pb-12">
+            <img src="/light-logo.svg" alt="logo" className="w-6 h-6" />
+          </header>
+          <Routes>
+            <Route path="/:id" element={<Profile />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
