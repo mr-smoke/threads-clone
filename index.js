3fee8e3ee33753cd7bc48fba05bb01b357e82c6f
baki duman <g.s_baki_70@hotmail.com>
2024-11-01 14:11:36 +0300

    Set up Express middleware and routing for user and post APIs.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./backend/db/connection.js";
+import userRoutes from "./backend/routes/user.route.js";
+import postRoutes from "./backend/routes/post.route.js";
 
 dotenv.config();
 connectDB();
@@ -11,6 +14,13 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
