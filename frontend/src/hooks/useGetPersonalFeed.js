af510c7e0a7a2252b6361ce4d6dbeb178c2434cf
baki duman <g.s_baki_70@hotmail.com>
2024-11-08 09:44:46 +0300

    Add personal feed fetching functionality; implement useGetPersonalFeed hook and enhance error handling with toasts.

---
+++ frontend/src/hooks/useGetPersonalFeed.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+
+const useGetPersonalFeed = () => {
+  const [isLoading, setIsLoading] = useState();
+  const [feed, setFeed] = useState();
+  const { toast } = useToast();
+
+  useEffect(() => {
+    const fetchFeed = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          "http://localhost:3000/api/post/personalFeed",
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
+        const data = await response.json();
+        setFeed(data);
+      } catch (error) {
+        toast({
+          description: error.message,
+        });
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchFeed();
+  }, []);
+
+  return { feed, isLoading };
+};
+
+export default useGetPersonalFeed;
