bcc580f14d7dfba70f633e05619f10c092ad2c2e
baki duman <g.s_baki_70@hotmail.com>
2024-11-27 17:19:23 +0300

    Add messaging functionality: implement sendMessage and getMessages endpoints; update getConversations to use user ID from request.

--- backend/controllers/chat.controller.js
+++ backend/controllers/chat.controller.js
@@ -1,9 +1,53 @@
 import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
 export const getConversations = async (req, res) => {
+  const userId = req.user._id;
+
   try {
-    const conversations = await Conversation.find({ userIds: req.userId });
+    const conversations = await Conversation.find({ userIds: userId });
     res.json(conversations);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  const userId = req.user._id;
+  const receiverId = req.params.receiverId;
+
+  try {
+    let conversation = await Conversation.findOne({
+      userIds: { $all: [userId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = new Conversation({
+        userIds: [userId, receiverId],
+      });
+      await conversation.save();
+    }
+
+    const newMessage = new Message({
+      conversationId: conversation._id,
+      senderId: userId,
+      content: req.body.text,
+    });
+
+    newMessage.save();
+
+    res.json(newMessage);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  const conversationId = req.params.conversationId;
+
+  try {
+    const messages = await Message.find({ conversationId }).createdAt("asc");
+    res.json(messages);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- backend/routes/chat.route.js
+++ backend/routes/chat.route.js
@@ -1,9 +1,15 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import { getConversations } from "../controllers/chat.controller.js";
+import {
+  getConversations,
+  sendMessage,
+  getMessages,
+} from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getConversations);
+router.post("/sendMessage/:receiverId", protectRoute, sendMessage);
+router.get("/getMessages/:conversationId", protectRoute, getMessages);
 
 export default router;
